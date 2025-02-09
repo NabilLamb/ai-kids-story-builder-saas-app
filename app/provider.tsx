cfaa3e59b736d96451461387ae7631cf12c3991c
Nabil Lambattan <lambattannabil2000@gmail.com>
2025-02-09 19:38:03 +0100

    fix: rename next.config.js and update tsconfig

--- app/provider.tsx
+++ app/provider.tsx
@@ -4,8 +4,9 @@ import React from "react";
 import { HeroUIProvider } from "@heroui/react";
 import { ClerkProvider } from "@clerk/nextjs";
 
+
 const Provider = ({ children }: { children: React.ReactNode }) => {
-  const clerkPubKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY; // Access env var
+  const clerkPubKey = "pk_test_ZW5kbGVzcy1idWNrLTcxLmNsZXJrLmFjY291bnRzLmRldiQ"; // Access env var
 
   if (!clerkPubKey) {
     console.error("Clerk publishable key is missing!"); // Important!
